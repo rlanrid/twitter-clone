b417e204c4cc24348c2975c8fc51a1ffae040f72
rlan <rladnrider77@gmail.com>
2024-11-27 20:23:31 +0900

    1127

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,3 +1,4 @@
+import Notification from "../models/notification.model.js";
 import User from "../models/user.model.js";
 
 export const getUserProfile = async (req, res) => {
@@ -38,10 +39,46 @@ export const followUnfollowUser = async (req, res) => {
       await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
       await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
       // Send notification to the user
+      const newNotification = new Notification({
+        type: "follow",
+        from: req.user._id,
+        to: userToModify._id,
+      });
+
+      await newNotification.save();
+
+      // TODO: return the id of the user as a response
       res.status(200).json({ message: "User followed successfully" });
     }
   } catch (error) {
     console.log("Error in getUserProfile: ", error.message);
     res.status(500).json({ error: error.message });
   }
-}
+};
+
+export const getSuggestedUsers = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const usersFollowedByMe = await User.findById(userId).select("following");
+
+    const users = await User.aggregate([
+      {
+        $match: {
+          _id: { $ne: userId }
+        },
+      },
+      { $sample: { size: 10 } }
+    ])
+
+    const filteredUsers = users.filter(user => !usersFollowedByMe.following.includes(user._id))
+    const suggestedUsers = filteredUsers.slice(0, 4)
+
+    suggestedUsers.forEach(user => user.password = null)
+
+    res.status(200).json(suggestedUsers);
+  } catch (error) {
+    console.log("Error in getSuggestedUsers: ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
