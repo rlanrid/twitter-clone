dab0778b756f27bb4003d855e8348eb0ee12fe84
rlan <rladnrider77@gmail.com>
2024-12-18 19:24:48 +0900

    1218

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
+import { Routes, Route } from "react-router-dom"
+
 import LoginPage from "./pages/auth/login/LoginPage"
 import SignUpPage from "./pages/auth/signup/SignUpPage"
 import HomePage from "./pages/home/HomePage"
-import { Routes, Route } from "react-router-dom"
+import NotificationPage from "./pages/notification/NotificationPage"
+import ProfilePage from "./pages/profile/ProfilePage"
+
+import RightPanel from "./components/common/RightPanel"
+import Sidebar from "./components/common/Sidebar"
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      {/* Routes에 감싸지 않는 일반 컴포넌트 */}
+      <Sidebar />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/notifications' element={<NotificationPage />} />
+        <Route path='/profile/:username' element={<ProfilePage />} />
       </Routes>
+      <RightPanel />
     </div>
   )
 }
