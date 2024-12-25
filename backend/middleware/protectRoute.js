eeb23a3caca771a0d1e162bf82609109be3deb76
rlan <rladnrider77@gmail.com>
2024-12-25 21:29:52 +0900

    1225

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 
 export const protectRoute = async (req, res, next) => {
   try {
+    // 토큰 생성
     const token = req.cookies.jwt;
     if (!token) {
       return res.status(401).json({ error: "로그인이 필요합니다." });
